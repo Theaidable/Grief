afc00b03aea1e3724d0c0251d327b2a3e807c43f
David Gudmund <dada5@student.eadania.dk>
2025-05-21 11:11:41 +0200

    Level1
    Level 1 burde være klar til at blive implementeret, skal ligges i mgcb og indlæses først

---
+++ Classes/Levels/Level.cs
@@ -0,0 +1,77 @@
+﻿using Grief.Classes.DesignPatterns.Builder.Builders;
+using Grief.Classes.DesignPatterns.Builder;
+using Grief.Classes.DesignPatterns.Composite;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.Tiled;
+using MonoGame.Extended.Tiled.Renderers;
+using System.Collections.Generic;
+
+namespace Grief.Classes.Levels
+{
+    public class Level
+    {
+        private TiledMap map;
+        private TiledMapRenderer mapRenderer;
+
+        public List<GameObject> GameObjects { get; private set; } = new List<GameObject>();
+
+        public void Load(string levelName)
+        {
+            map = GameWorld.Instance.Content.Load<TiledMap>(levelName);
+            mapRenderer = new TiledMapRenderer(GameWorld.Instance.GraphicsDevice, map);
+
+            switch (levelName)
+            {
+                case "Level0":
+                    //Her kan vi lave koden til en main menu
+                    break;
+                case "Level1":
+                    GameObjects.Add(CreatePlayer(new Vector2(100,400))); //Sæt positionen for spilleren her
+                    //Vi kan tilføje flere GameObjects i Level 1 her
+                    break;
+            }
+        }
+
+        private GameObject CreatePlayer(Vector2 position)
+        {
+            PlayerBuilder playerBuilder = new PlayerBuilder();
+            GameObjectDirector director = new GameObjectDirector(playerBuilder);
+            var player = director.Construct("Player");
+            playerBuilder.SetPosition(position);
+            return player;
+        }
+
+        public void AddGameObject(GameObject gameObject)
+        {
+            GameObjects.Add(gameObject);
+            gameObject.Awake();
+            gameObject.Start();
+        }
+
+        public void RemoveGameObject(GameObject gameObject)
+        {
+            GameObjects.Remove(gameObject);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            mapRenderer.Update(gameTime);
+
+            foreach (GameObject gameObject in GameObjects)
+            {
+                gameObject.Update();
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            mapRenderer.Draw();
+
+            foreach (GameObject gameObject in GameObjects)
+            {
+                gameObject.Draw(spriteBatch);
+            }
+        }
+    }
+}
